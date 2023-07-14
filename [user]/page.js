3a51865fa9da7139a5da3759d92b50c41e23eb5b
iankit-tanwar <tankitofficial01.gmail.com>
2023-07-14 15:57:07 +0530

    vommit

---
+++ [user]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page({params:{user }}) {
+  return (
+    <div>user page {user} </div>
+  )
+}
---
+++ blog/[slug]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page({params:{slug}}) {
+  return (
+    <div>slug {slug}</div>
+  )
+}
